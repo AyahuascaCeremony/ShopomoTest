13a8bdb0c5567cc3eb0ba1a5a701a6c59694c283
Richard Rodriguez <richard.rodriguez@comparethemarket.com>
2016-04-11 20:42:23 +0100

    1 - First test, describes the API for the Password Generator

---
+++ VeInteractiveAssessment.OneTimePassword.Tests/GeneratingAndValidatingPasswords.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace VeInteractiveAssessment.OneTimePassword.Tests
+{
+    public class GeneratingAndValidatingPasswords
+    {
+        [Test]
+        public void GeneratedPasswordIsValidForTheCorrectUserId()
+        {
+            var passwordGenerator = new OneTimePasswordGenerator();
+            var userId = "User01";
+
+            var password = passwordGenerator.GenerateFor(userId);
+
+            Assert.That(passwordGenerator.Validate(userId, password), Is.True);
+        }
+    }
+}
---
+++ VeInteractiveAssessment.OneTimePassword/OneTimePasswordGenerator.cs
@@ -0,0 +1,15 @@
+namespace VeInteractiveAssessment.OneTimePassword
+{
+    public class OneTimePasswordGenerator
+    {
+        public string GenerateFor(string userId)
+        {
+            return string.Empty;
+        }
+
+        public bool Validate(string userId, string password)
+        {
+            return true;
+        }
+    }
+}
