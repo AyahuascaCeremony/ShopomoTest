3f4222c3a55c4aa41c0f7569fd0495f3ae52f46d
Richard Rodriguez <richard.rodriguez@comparethemarket.com>
2016-05-02 20:56:03 +0100

    Provide example implementation of IAuditLoginFailures

---
+++ VeInteractiveAssessment.OneTimePassword/ConsoleAuditer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VeInteractiveAssessment.OneTimePassword
+{
+    class ConsoleAuditer : IAuditLoginFailures
+    {
+        public void Audit(string userId, DateTime dateTime)
+        {
+            Console.WriteLine("Failed login attempt for UserId: {0} at: {1}", userId, dateTime);
+        }
+    }
+}
